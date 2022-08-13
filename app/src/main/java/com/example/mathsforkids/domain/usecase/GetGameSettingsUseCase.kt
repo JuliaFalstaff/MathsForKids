1106877f7bb1018e0ce2944fddef83919ec6c9dc
julia <j.kholstinina@yandex.ru>
2022-08-13 21:57:28 +0300

    add domain

--- app/src/main/java/com/example/mathsforkids/domain/usecase/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/example/mathsforkids/domain/usecase/GetGameSettingsUseCase.kt
@@ -5,7 +5,6 @@ import com.example.mathsforkids.domain.entity.Level
 import com.example.mathsforkids.domain.repository.Repository
 
 class GetGameSettingsUseCase(private val repository: Repository) {
-
     operator fun invoke(level: Level): GameSettings {
         return repository.getGameSettings(level)
     }
